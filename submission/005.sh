537a802a6f6125777e01a1f1b931363e09728245
stmf-rema <stmf.000@gmail.com>
2024-11-24 13:37:14 -0300

    test 1-6

--- submission/005.sh
+++ submission/005.sh
@@ -1,2 +1,26 @@
 # Create a 1-of-4 P2SH multisig address from the public keys in the four inputs of this tx:
 #   `37d966a263350fe747f1c606b159987545844a493dd38d84b070027a895c4517`
+transactions=$(bitcoin-cli getrawtransaction "37d966a263350fe747f1c606b159987545844a493dd38d84b070027a895c4517" true)
+
+#echo $transactions
+
+#tx1=$(echo "$transactions" | jq -r .vin.[0].txid)
+#tx1Witness=$(echo "$transactions" | jq -r .vin.[0].txinwitness)
+#tx2=$(echo "$transactions" | jq -r .vin.[1].txid)
+#tx2Witness=$(echo "$transactions" | jq -r .vin.[1].txinwitness)
+#tx3=$(echo "$transactions" | jq -r .vin.[2].txid)
+#tx3Witness=$(echo "$transactions" | jq -r .vin.[2].txinwitness)
+#tx4=$(echo "$transactions" | jq -r .vin.[3].txid)
+#tx4Witness=$(echo "$transactions" | jq -r .vin.[3].txinwitness)
+
+tx1PubKey=$(echo "$transactions" | jq .vin.[0].txinwitness.[1])
+tx2PubKey=$(echo "$transactions" | jq .vin.[1].txinwitness.[1])
+tx3PubKey=$(echo "$transactions" | jq .vin.[2].txinwitness.[1])
+tx4PubKey=$(echo "$transactions" | jq .vin.[3].txinwitness.[1])
+
+#echo $tx1
+#echo $tx1Witness
+#echo $tx1PubKey
+
+bitcoin-cli createmultisig 1 "[$tx1PubKey,$tx2PubKey,$tx3PubKey,$tx4PubKey]" "legacy" | jq -r .address
+
--- submission/006.sh
+++ submission/006.sh
@@ -1,1 +1,34 @@
 # Which tx in block 257,343 spends the coinbase output of block 256,128?
+
+# Step 1: get all transactions from block 256128
+blockHash=$(bitcoin-cli getblockhash "256128")
+#echo $blockHash
+block=$(bitcoin-cli getblock $blockHash)
+#echo $block
+
+# Step 2: get the coinbase transaction (the first output)
+coinbaseTxId=$(echo $block |  jq -r .tx.[0])
+# echo "coinbaseTxId $coinbaseTxId"
+# coinbaseTx=$(./bitcoin-cli  getrawtransaction $coinbaseTxId true)
+# echo $coinbaseTx
+
+# Step 3: get all transactions from block 257343
+blockHash=$(bitcoin-cli getblockhash "257343")
+#echo $blockHash
+block=$(bitcoin-cli getblock $blockHash)
+#echo $block
+
+# Step 4: iterate in all transactions to find the previous address as the input
+numberOfTransactions=$(echo $block | jq -r .nTx)
+for i in $(seq 1 $((numberOfTransactions - 1))); do # skip the first since it is the coinbase
+  txId=$(echo $block |  jq -r .tx.[$i])
+  tx=$(bitcoin-cli getrawtransaction $txId true)
+  vinLength=$(echo $tx | jq -r '.vin | length')
+  for j in $(seq 0 $((vinLength - 1))); do
+    vinTxId=$(echo "$tx" | jq -r ".vin.[$j].txid")
+    if [ "$vinTxId" == "$coinbaseTxId" ]; then
+      echo $txId
+      exit
+    fi
+  done  
+done
